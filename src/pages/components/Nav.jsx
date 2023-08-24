02ad46be6e8480403673a265dfe2776767791b5c
ManI <letz0703@gmail.com>
2023-08-25 02:44:18 +0900

    svg

--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -1,12 +1,25 @@
+import {useState} from "react"
 import cloud from "/cloud.jpeg"
 import {motion} from "framer-motion"
 
 export default function Nav() {
+  const [toggled, setToggled] = useState(false)
   return (
-    <>
-      <nav>
-        <h1>Logo</h1>
-      </nav>
-    </>
+    <nav className={`relatie mx-8 mb-24`}>
+      <svg
+        width="250"
+        height="4"
+        viewBox="0 0 250 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2 2L428 2"
+          stroke="#282828"
+          strokeLinecap="round"
+          strokeWidth={2}
+        />
+      </svg>
+    </nav>
   )
 }
