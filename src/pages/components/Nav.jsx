afd6d1218383d441ea5210c33a1be29beb22a52e
ManI <letz0703@gmail.com>
2023-08-26 05:42:25 +0900

    design ham

--- src/pages/components/Nav.jsx
+++ src/pages/components/Nav.jsx
@@ -8,46 +8,60 @@ export default function Nav() {
   const [toggled, setToggled] = useState(false)
   const size = useMediaQuery("(min-width:1280px)")
   return (
-    <>
-      <nav
-        className={`relative  mx-4 mb-24 flex justify-between items-center pt-8 pb-3 font-medium md:mx-16 lg:mx-32`}
+    <nav
+      className={`relative  mx-4 mb-24 flex justify-between items-center pt-8 pb-3 font-medium md:mx-16 lg:mx-32`}
+    >
+      <svg
+        className={`absolute bottom-0 left-1/2 -translate-x-1/2 `}
+        width="250"
+        height={4}
+        viewBox="0 0 250 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          className={`absolute bottom-0 left-1/2 -translate-x-1/2 `}
-          width="250"
-          height={4}
-          viewBox="0 0 250 4"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M2 2L428 2"
-            strokeWidth={2}
-            stroke="#282828"
-            strokeLinecap="round"
-          />
-        </svg>
-        <div>
-          <img src={logo} alt="" className={`h-[50px]`} />
+        <path
+          d="M2 2L428 2"
+          strokeWidth={2}
+          stroke="#282828"
+          strokeLinecap="round"
+        />
+      </svg>
+      <div>
+        <img src={logo} alt="" className={`h-[50px]`} />
+      </div>
+      {/*{!size && (*/}
+      <h1 className={`text-lg font-bold`}>
+        <a href="/" className={`text-2xl`}>
+          i.etz{" "}
+        </a>
+      </h1>
+      {/*)}*/}
+      {/* View Desktop Vs. Mobile */}
+      {size && (
+        <div className={`flex gap-12`}>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/shop">Shop</a>
         </div>
-        {size && (
-          <h1 className={`text-lg font-bold`}>
-            <a href="/" className={`text-2xl`}>
-              i.etz{" "}
-            </a>
-          </h1>
-        )}
-        {!size && (
-          <div
-            className={`space-y-2 cursor-pointer`}
-            onClick={() => setToggled(prev => !prev)}
-          >
-            <span className={`block h-0.5 w-8 bg-black`}></span>
-            <span className={`block h-0.5 w-8 bg-black`}></span>
-            <span className={`block h-0.5 w-8 bg-black`}></span>
-          </div>
-        )}
-      </nav>
-    </>
+      )}
+      {!size && (
+        <div
+          className={`space-y-2 cursor-pointer`}
+          onClick={() => setToggled(prev => !prev)}
+        >
+          <span className={`block h-0.5 w-8 bg-black`}></span>
+          <span className={`block h-0.5 w-8 bg-black`}></span>
+          <span className={`block h-0.5 w-8 bg-black`}></span>
+        </div>
+      )}
+
+      {toggled && (
+        <div className={`flex gap-12`}>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/shop">Shop</a>
+        </div>
+      )}
+    </nav>
   )
 }
